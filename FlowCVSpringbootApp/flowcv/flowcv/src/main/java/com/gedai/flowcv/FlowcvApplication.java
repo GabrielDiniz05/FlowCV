b08bbf5078468f4d7fdb5b45320c9509683e2b06
Gabriel Diniz <gabriel.assisdiniz@gmail.com>
2023-09-23 21:30:20 -0300

    AjustandoProjeto

---
+++ FlowCVSpringbootApp/flowcv/flowcv/src/main/java/com/gedai/flowcv/FlowcvApplication.java
@@ -0,0 +1,13 @@
+package com.gedai.flowcv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowcvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowcvApplication.class, args);
+	}
+
+}
