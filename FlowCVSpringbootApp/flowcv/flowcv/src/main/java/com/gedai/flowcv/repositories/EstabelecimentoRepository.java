ed20705f958928b2b46b3f83abab15f2c758913a
Gabriel Diniz <gabriel.assisdiniz@gmail.com>
2023-10-25 12:59:21 -0300

    Correção

--- FlowCVSpringbootApp/flowcv/flowcv/src/main/java/com/gedai/flowcv/repositories/EstabelecimentoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gedai.flowcv.repositories;
-
-import com.gedai.flowcv.models.Estabelecimento;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EstabelecimentoRepository extends JpaRepository<Estabelecimento, Integer> {
-}
